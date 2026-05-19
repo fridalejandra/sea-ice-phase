768c1f7436c4ed37e70e2570ba0acddc5eac99e7
fridalejandra <falejandraperez@ucla.edu>
2026-05-19 09:31:18 -0700

    SIE anom matrix -col

--- scripts/python/plotting/Ch3/figures/fig_monthly_SIE_corr_matrix.py
+++ scripts/python/plotting/Ch3/figures/fig_monthly_SIE_corr_matrix.py
@@ -115,8 +115,8 @@ def correlation_matrix(mon_df):
             if len(paired) < 10:
                 continue
             r, p = spearmanr(paired[m1], paired[m2])
-            r_mat[i, j] = r
-            p_mat[i, j] = p
+            r_mat[i, j] = float(r)
+            p_mat[i, j] = float(p)
     return r_mat, p_mat
 
 
